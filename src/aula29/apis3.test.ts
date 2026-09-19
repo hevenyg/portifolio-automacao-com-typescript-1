3998730e94d2e42f97a4a0ca5d2f9cace7dd78ff
Heveny <169173325+hevenyg@users.noreply.github.com>
2026-09-19 12:18:26 +0000

    Atualização da aula 29

--- src/aula29/apis3.test.ts
+++ src/aula29/apis3.test.ts
@@ -24,3 +24,30 @@ test('Método para criar um novo post', async () => {
   expect(dados.title).toBe('Meu novo POST DA ATIVIDADE DE PUT');
   expect(dados.body).toBe('Conteúdo ATIVIDADE PUT');
 });
+
+test('Metodo PATCH para ATUALIZAR um  post', async () =>{
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+        method:'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            title: 'MEU TITULO SUPER ATUALIZADO',
+        })
+    });
+    // Testa status code 
+        expect(res.status).toBe(200);
+    // Testa se o retorno é um objeto JSON
+        const dados = await res.json();
+        expect(dados.title).toBe('MEU TITULO SUPER ATUALIZADO');
+        // expect(dados.body).toBe('Conteudo do meu novo post');
+});
+
+test('Metodo DELETE para DELETAR um  post', async () =>{
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+        method:'DELETE',
+    });
+    // Testa status code 
+        expect(res.status).toBe(200);
+});
+
